afd2cec60c35a80a822d2e46c59fc918913dee8c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2020-04-27 16:41:27 +0200

    Add date formatting

--- api/program.ts
+++ api/program.ts
@@ -1,18 +1,46 @@
 import { NowRequest, NowResponse } from "@now/node";
 import Airtable from "airtable";
 
+interface Event {
+  jmeno: string;
+  datum: string;
+  info: string;
+  fb: string | null;
+  promo: boolean;
+  zverejnit: boolean;
+}
+
 export default async (_: NowRequest, response: NowResponse) => {
   const apiKey = process.env.AIRTABLE_API_KEY;
   const table = new Airtable({ apiKey }).base("appKjB9jkVXK4YRGJ")("Program");
   try {
     const records = await table.select({ view: "Grid view" }).all();
-    const wanted = records
-      .map((r) => r.fields)
-      .filter((r) => r["Zveřejnit"] === true);
-    const out = JSON.stringify(wanted, null, 2);
+    const events = records.map(parseRecord).filter((e) => e.zverejnit);
+    const out = JSON.stringify(events, null, 2);
     response.setHeader("Content-Type", "application/json");
     response.status(200).send(out);
   } catch (err) {
     response.status(500).send("AirTable read error.");
   }
 };
+
+function parseRecord(record: Airtable.Record<{}>): Event {
+  const options = {
+    weekday: "long",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const formatDate = (s: string) =>
+    new Date(s).toLocaleDateString("cs-CZ", options);
+  const f = record.fields;
+  return {
+    jmeno: f["Název"],
+    datum: f["Kdy"] || formatDate(f["Kdy přesně"]),
+    info: f["Popis"],
+    fb: f["FB událost"],
+    promo: f["Promovat"],
+    zverejnit: f["Zveřejnit"],
+  };
+}
