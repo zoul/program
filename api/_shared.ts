567f82edc492db11133e68b97be6b0988aa87f5e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2020-05-02 17:19:17 +0200

    Add a programme endpoint for Zpravodaj

--- api/_shared.ts
+++ api/_shared.ts
@@ -3,6 +3,7 @@ import Airtable from "airtable";
 export interface Event {
   jmeno: string;
   datum: string;
+  datumPresne: Date | null;
   info: string;
   fb: string | null;
   promo: boolean;
@@ -23,9 +24,14 @@ export function parseEvent(record: Airtable.Record<{}>): Event {
   return {
     jmeno: f["Název"],
     datum: f["Kdy"] || formatDate(f["Kdy přesně"]),
+    datumPresne: map((x) => new Date(x), f["Kdy přesně"]),
     info: f["Popis"],
     fb: f["FB událost"],
     promo: f["Promovat"],
     zverejnit: f["Zveřejnit"],
   };
+}
+
+function map<T, U>(f: (t: T) => U, val: T | null): U | null {
+  return val != null ? f(val) : null;
 }
---
+++ api/zpravodaj.ts
@@ -0,0 +1,46 @@
+import { NowRequest, NowResponse } from "@now/node";
+import { parseEvent } from "./_shared";
+import Airtable from "airtable";
+
+export default async (_: NowRequest, response: NowResponse) => {
+  const apiKey = process.env.AIRTABLE_API_KEY;
+  const table = new Airtable({ apiKey }).base("appKjB9jkVXK4YRGJ")("Program");
+  try {
+    const records = await table.select({ view: "Budoucí akce" }).all();
+    const events = records.map(parseEvent).filter((e) => e.zverejnit);
+    const print = (s: string) => response.write(`${s}\n`);
+    response.setHeader("Content-Type", "text/plain");
+    response.status(200);
+    for (const event of events) {
+      const date = event.datumPresne;
+      if (date == null) {
+        continue;
+      }
+      print(formatDate(event.datumPresne));
+      print(`Prostor ${formatTime(event.datumPresne)}`);
+      print(event.jmeno);
+      print(event.info);
+      print("\n");
+    }
+    response.end();
+  } catch (err) {
+    response.status(500).send("AirTable read error.");
+  }
+};
+
+function formatDate(d: Date): string {
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+  return d.toLocaleDateString("cs-CZ", options);
+}
+
+function formatTime(d: Date): string {
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+  return d.toLocaleTimeString("cs-CZ", options);
+}
