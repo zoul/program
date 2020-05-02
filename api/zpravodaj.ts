faf256042952bbf89f7172699b67a2997819655a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2020-05-02 17:22:39 +0200

    Add explicit encoding info to the Zpravodaj endpoint

--- api/zpravodaj.ts
+++ api/zpravodaj.ts
@@ -9,7 +9,7 @@ export default async (_: NowRequest, response: NowResponse) => {
     const records = await table.select({ view: "Budoucí akce" }).all();
     const events = records.map(parseEvent).filter((e) => e.zverejnit);
     const print = (s: string) => response.write(`${s}\n`);
-    response.setHeader("Content-Type", "text/plain");
+    response.setHeader("Content-Type", "text/plain; charset=UTF-8");
     response.status(200);
     for (const event of events) {
       const date = event.datumPresne;
